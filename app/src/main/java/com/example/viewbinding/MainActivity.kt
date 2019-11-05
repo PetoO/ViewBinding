054cfda9222fa80e6322a7518773a4eecef7f927
Peter Zapalac <zapalac.peter@gmail.com>
2019-11-05 19:21:03 +0100

    Simple view binding example

--- app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.viewbinding
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainTextView.text = getString(R.string.binded_text)
     }
-
-
 }
